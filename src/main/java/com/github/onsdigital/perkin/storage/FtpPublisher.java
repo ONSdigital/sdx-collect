7aabf744f4dc623a0204856a5f2d34318905e2e7
David Gunthorpe <mail@davidgunthorpe.co.uk>
2016-02-29 15:23:41 +0000

    using fake ftp server for publish unit tests

--- src/main/java/com/github/onsdigital/perkin/storage/FtpPublisher.java
+++ src/main/java/com/github/onsdigital/perkin/storage/FtpPublisher.java
@@ -14,28 +14,57 @@ import java.io.IOException;
  */
 public class FtpPublisher implements Publisher {
 
+    private String host;
+    private int port;
+    private String user;
+    private String password;
+    private String remoteDirectory;
+
+    public FtpPublisher() {
+        //TODO defaults via system properties?
+        setHost("192.168.99.100");
+        setPort(21);
+        setUser("ons");
+        setPassword("ons");
+        setRemoteDirectory("/");
+    }
+
     public void publish(final FileItem data, final String path) throws IOException {
         String filename = data.getName();
         ftpFile(data.getInputStream(), path, filename);
     }
 
-    private void ftpFile(InputStream inputStream, String path, String filename) throws IOException {
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-        String serverAddress = "192.168.99.100";
-        String userId = "ons";
-        String password = "ons";
-        //TODO: ignoring path parameter for now
-        String remoteDirectory = "/";
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRemoteDirectory(String remoteDirectory) {
+        this.remoteDirectory = remoteDirectory;
+    }
+
+    private void ftpFile(InputStream inputStream, String path, String filename) throws IOException {
 
         //new ftp client
         FTPClient ftp = new FTPClient();
         //try to connect
-        System.out.println("ftp connect to server: " + serverAddress);
-        ftp.connect(serverAddress);
+        System.out.println("ftp connect to " + host + " " + port);
+        ftp.connect(host, port);
 
         //login to server
-        System.out.println("ftp login userId: " + userId);
-        if (ftp.login(userId, password)) {
+        System.out.println("ftp login user: " + user);
+        if (ftp.login(user, password)) {
 
             int reply = ftp.getReplyCode();
             //FTPReply stores a set of constants for FTP reply codes.
@@ -64,8 +93,10 @@ public class FtpPublisher implements Publisher {
                 ftp.disconnect();
             }
         } else {
-            System.out.println("ftp login failed");
+            String msg = "ftp login to " + host + ":" + port + " failed for user: " + user;
+            System.out.println(msg);
             ftp.logout();
+            throw new IOException(msg);
         }
     }
 }
---
+++ src/test/java/com/github/onsdigital/perkin/storage/FtpPublisherTest.java
@@ -0,0 +1,119 @@
+package com.github.onsdigital.perkin.storage;
+
+import com.github.onsdigital.perkin.helpers.Json;
+import com.github.onsdigital.perkin.json.Survey;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockftpserver.fake.FakeFtpServer;
+import org.mockftpserver.fake.UserAccount;
+import org.mockftpserver.fake.filesystem.DirectoryEntry;
+import org.mockftpserver.fake.filesystem.FileSystem;
+import org.mockftpserver.fake.filesystem.UnixFakeFileSystem;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.Mockito.when;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FtpPublisherTest {
+
+    @Mock
+    private FileItem data;
+
+    private FtpPublisher classUnderTest;
+
+    private FakeFtpServer fakeFtpServer;
+    private int port;
+
+    @Before
+    public void setUp() throws IOException {
+        //example json file
+        String json = Json.format(Survey.builder().id("id").respondentId("respondentId").build());
+        InputStream in = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+        when(data.getInputStream()).thenReturn(in);
+        when(data.getName()).thenReturn("test.json");
+
+        classUnderTest = new FtpPublisher();
+
+        fakeFtpServer = new FakeFtpServer();
+        fakeFtpServer.setServerControlPort(0);  // use any free port
+        fakeFtpServer.addUserAccount(new UserAccount("ons", "ons", "/"));
+
+        FileSystem fileSystem = new UnixFakeFileSystem();
+        fileSystem.add(new DirectoryEntry("/"));
+        fakeFtpServer.setFileSystem(fileSystem);
+
+        fakeFtpServer.start();
+        port = fakeFtpServer.getServerControlPort();
+
+        classUnderTest.setHost("localhost");
+        classUnderTest.setPort(port);
+
+        System.out.println("FakeFtpServer running on port " + port);
+    }
+
+    @After
+    public void tearDown() {
+        fakeFtpServer.stop();
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldErrorConnectionRefused() throws IOException {
+        //given
+        classUnderTest.setPort(8888);
+
+        //when
+        classUnderTest.publish(data, "");
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldErrorInvalidCredentials() throws IOException {
+        //given
+        classUnderTest.setUser("invalid");
+
+        //when
+        classUnderTest.publish(data, "");
+    }
+
+    @Test
+    public void shouldPublishFile() throws IOException {
+        //given
+
+        //when
+        classUnderTest.publish(data, "");
+
+        //then
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect("localhost", port);
+        ftpClient.login("ons", "ons");
+        FTPFile[] files = ftpClient.listFiles();
+
+        for (FTPFile file : files) {
+            System.out.println("FtpPublisherTest - ftp list. file: " + file.getName());
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ftpClient.retrieveFile(files[0].getName(), out);
+        String contents = out.toString("UTF-8");
+        System.out.println("test.json: " + contents);
+        ftpClient.quit();
+        ftpClient.disconnect();
+
+        assertThat(files.length, is(1));
+        assertThat(files[0].getName(), is("test.json"));
+    }
+}
