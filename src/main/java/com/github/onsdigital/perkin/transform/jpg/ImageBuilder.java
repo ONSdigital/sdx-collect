a1fed2c726b5e440fe5bbabcbc8c5dd7df62d7e7
David Gunthorpe <mail@davidgunthorpe.co.uk>
2016-03-08 15:30:40 +0000

    publish jpg image files

--- src/main/java/com/github/onsdigital/perkin/transform/jpg/ImageBuilder.java
+++ src/main/java/com/github/onsdigital/perkin/transform/jpg/ImageBuilder.java
@@ -11,10 +11,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 /**
