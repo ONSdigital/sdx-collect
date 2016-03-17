b24d238ff1ecd884ef95620a0b6ac012a88d2592
David Gunthorpe <mail@davidgunthorpe.co.uk>
2016-03-17 11:02:19 +0000

    initial image index csv

---
+++ src/main/java/com/github/onsdigital/perkin/transform/jpg/ImageIndexCsv.java
@@ -0,0 +1,27 @@
+package com.github.onsdigital.perkin.transform.jpg;
+
+import com.github.onsdigital.perkin.transform.DataFile;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * An IDBR receipt records that a respondent unit (RU) has completed a survey (id)
+ * on a particular date.
+ */
+@Data
+@Builder
+public class ImageIndexCsv implements DataFile {
+
+    private String csv;
+    private String filename;
+
+    @Override
+    public byte[] getBytes() {
+        return csv.getBytes();
+    }
+
+    @Override
+    public long getSize() {
+        return csv.length();
+    }
+}
