8a92b7e8b37840f1a3b7d385ee0803e1dd48de7b
David Carboni <david@carboni.io>
2016-04-25 16:19:05 +0100

    Added functionality to load saved sequence numbers.

--- src/main/java/com/github/onsdigital/perkin/transform/NumberService.java
+++ src/main/java/com/github/onsdigital/perkin/transform/NumberService.java
@@ -2,6 +2,8 @@ package com.github.onsdigital.perkin.transform;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -26,8 +28,8 @@ public class NumberService {
         this.start = start;
         this.end = end;
 
-        //TODO: see if file exists for initial value
         number = new AtomicLong(start);
+        load(start, end);
     }
 
     public long getNext() {
@@ -43,16 +45,41 @@ public class NumberService {
     }
 
     public void save() {
-        try {
-            FileOutputStream out = new FileOutputStream("./" + name + ".sequence");
+        try (FileOutputStream out = new FileOutputStream("./" + name + ".sequence")) {
 
             Properties properties = new Properties();
             properties.put(name, "" + number.get());
 
             properties.store(out, "saved on shutdown");
-            out.close();
         } catch (IOException e) {
             log.error("SEQUENCE|problem saving sequence: {} value: {}", name, number.get());
+        }
+    }
+
+    /**
+     * Gracefully loads a saved sequence value, ensuring it is in the start/end range.
+     *
+     * @param start The minimum value.
+     * @param end   The maximum value.
+     */
+    public void load(long start, long end) {
+        String value = null;
+        File file = new File("./" + name + ".sequence");
+        if (file.exists()) {
+            try (FileInputStream in = new FileInputStream(file)) {
+
+                Properties properties = new Properties();
+                properties.load(in);
+                value = properties.getProperty(name, String.valueOf(number.get()));
+                long sequence = Math.max(Long.parseLong(value), start);
+                sequence = Math.min(sequence, end);
+                number.set(sequence);
+
+            } catch (IOException e) {
+                log.error("SEQUENCE|problem loading sequence: {} value: {}", name, number.get());
+            } catch (NumberFormatException e) {
+                log.error("SEQUENCE|problem loading sequence: {} value could not be parsed as a number: {}", name, value);
+            }
         }
     }
 }
