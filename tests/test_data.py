d068c04d246b13e4f2491cdff130e442e853acd0
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-07-26 17:21:45 +0100

    add tx_id to receipt

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,25 +2,25 @@ fake_encrypted = "abcdefghijklmnopqrstuvwxyz"
 
 valid_decrypted = '''
 {
+  "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
   "type": "uk.gov.ons.edc.eq:surveyresponse",
-  "origin": "uk.gov.ons.edc.eq",
-  "survey_id": "194825",
   "version": "0.0.1",
+  "origin": "uk.gov.ons.edc.eq",
+  "survey_id": "023",
   "collection": {
     "exercise_sid": "hfjdskf",
-    "instrument_id": "10",
-    "period": "0616"
+    "instrument_id": "0102",
+    "period": "1604"
   },
-  "submitted_at": "2016-03-12T10:39:40Z",
   "metadata": {
     "user_id": "789473423",
-    "ru_ref": "1234570071A"
+    "ru_ref": "12345678901A"
   },
   "data": {
-    "1": "2",
-    "2": "4",
-    "3": "2",
-    "4": "Y"
+    "11": "1/4/2016",
+    "12": "31/10/2016",
+    "20": "1800000",
+    "21": "60000"
   }
 }
 '''
