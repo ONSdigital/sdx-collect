d22862dde8ca93f0a26e609397db23b5e7a4a804
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-07-27 12:24:48 +0100

    no empty tx_id element

--- tests/test_receipt.py
+++ tests/test_receipt.py
@@ -35,10 +35,19 @@ class TestReceipt(unittest.TestCase):
         self.assertTrue("Basic " in headers['Authorization'])
         self.assertEqual(headers['Content-Type'], "application/vnd.collections+xml")
 
-    def test_render_xml_valid_json(self):
+    def test_render_xml_valid_json_txid(self):
         decrypted_json = json.loads(valid_decrypted)
         output_xml = receipt.get_receipt_xml(decrypted_json)
-        expected_xml = get_file_as_string("./tests/xml/valid_receipt.xml")
+        expected_xml = get_file_as_string("./tests/xml/receipt_txid.xml")
+
+        self.assertEqual(output_xml, expected_xml)
+
+    def test_render_xml_valid_json_no_txid(self):
+        decrypted_json = json.loads(valid_decrypted)
+        del decrypted_json['tx_id']
+
+        output_xml = receipt.get_receipt_xml(decrypted_json)
+        expected_xml = get_file_as_string("./tests/xml/receipt_no_txid.xml")
 
         self.assertEqual(output_xml, expected_xml)
 
