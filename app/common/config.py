ed15060ce204095399379c24527c72613ae17965
tundish <tundish@thuswise.org>
2016-12-14 11:24:56 +0000

    Added tests for check_safe_value.

--- app/common/config.py
+++ app/common/config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #   coding: UTF-8
 
+import re
+
 configTemplate = """
 [sdx.collect]
 secret = {secret}
@@ -14,11 +16,12 @@ secret = ${{sdx.collect:secret}}
 
 
 def check_safe_value(val):
-    return True
+    r = re.compile("[$#]")
+    return isinstance(val, str) and r.search(val) is None
 
 if __name__ == "__main__":
     import symmetric
 
-    data = {"secret": symmetric.generate_key()}
+    data = {"secret": symmetric.generate_key().decode("utf-8")}
     output = configTemplate.format(**data)
     print(output)
--- app/common/test/test_config.py
+++ app/common/test/test_config.py
@@ -8,7 +8,31 @@ from app.common.config import check_safe_value
 
 class ConfigTests(unittest.TestCase):
 
-    secretBytesWithDollar = b'SKbH0bRYIa9irkLYxPkAi68JSt0_M1x3lci4nCIK7Ec='
+    secretBytes = b"SKbH0bRYIa9irkLYxPkAi68JSt0_M1x3lci4nCIK7Ec="
+    secretBytesWithDollar = b"SKbH0bRYIa9irkLYxPkAi68J$t0_M1x3lci4nCIK7Ec="
+    secretBytesWithHash = b"SKb#0bRYIa9irkLYxPkAi68JSt0_M1x3lci4nCIK7Ec="
+    secretString = "xr37-bYBRm1HYsJXuoq1X-gkt3WUxqgiSALpzuvtlSc="
+    secretStringWithDollar = "xr37-bYBRm1HYsJXuoq1X-gkt3WUxqgiSALpzuvtl$c="
+    secretStringWithHash = "xr37-bYBRm1#YsJXuoq1X-gkt3WUxqgiSALpzuvtlSc="
 
-    def test_bad_bytes_dollar(self):
+    def test_bytes(self):
+        self.assertFalse(check_safe_value(ConfigTests.secretBytes))
+
+    def test_bytes_with_dollar(self):
+        self.assertIn(b"$", ConfigTests.secretBytesWithDollar)
         self.assertFalse(check_safe_value(ConfigTests.secretBytesWithDollar))
+
+    def test_bytes_with_hash(self):
+        self.assertIn(b"#", ConfigTests.secretBytesWithHash)
+        self.assertFalse(check_safe_value(ConfigTests.secretBytesWithHash))
+
+    def test_string(self):
+        self.assertTrue(check_safe_value(ConfigTests.secretString))
+
+    def test_string_with_dollar(self):
+        self.assertIn("$", ConfigTests.secretStringWithDollar)
+        self.assertFalse(check_safe_value(ConfigTests.secretStringWithDollar))
+
+    def test_string_with_hash(self):
+        self.assertIn("#", ConfigTests.secretStringWithHash)
+        self.assertFalse(check_safe_value(ConfigTests.secretStringWithHash))
