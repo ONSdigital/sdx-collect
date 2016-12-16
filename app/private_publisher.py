a105dd2e616a652053c0ddc31fe8ec1237edaf6d
tundish <tundish@thuswise.org>
2016-12-16 11:18:47 +0000

    Added tests for encryption, decryption, type conversion.

--- app/private_publisher.py
+++ app/private_publisher.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
 #   coding: UTF-8
 
-from queue_publisher import QueuePublisher
+from app.queue_publisher import QueuePublisher
 
 from cryptography.fernet import Fernet
 
 
 class PrivatePublisher(QueuePublisher):
+
+    @staticmethod
+    def encrypt(message, secret):
+        """
+        Message may be a string or bytes.
+        Secret key must be 32 url-safe base64-encoded bytes.
+
+        """
+        try:
+            f = Fernet(secret)
+        except ValueError:
+            return None
+        try:
+            token = f.encrypt(message)
+        except TypeError:
+            token = f.encrypt(message.encode("utf-8"))
+        return token
+
+    @staticmethod
+    def decrypt(token, secret):
+        """
+        Secret key must be 32 url-safe base64-encoded bytes
+
+        Returned value is a string.
+        """
+        try:
+            f = Fernet(secret)
+            message = f.decrypt(token)
+        except ValueError:
+            return None
+        return message.decode("utf-8")
 
     def publish_message(self, message, content_type=None, headers=None, secret=None):
         if isinstance(secret, str):
---
+++ tests/test_private_publisher.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#   coding: UTF-8
+
+import base64
+import unittest
+
+from app.private_publisher import PrivatePublisher
+
+
+class PrivatePublisherTests(unittest.TestCase):
+
+    def test_encrypt_bytes_message(self):
+        secret = base64.b64encode(b"x" * 32)
+        message = "Test string".encode("utf-8")
+        rv = PrivatePublisher.encrypt(message, secret=secret)
+        self.assertIsInstance(rv, bytes)
+        self.assertIsInstance(rv.decode("ascii"), str)
+        self.assertIsInstance(base64.urlsafe_b64decode(rv.decode("ascii")), bytes)
+
+    def test_encrypt_string_message(self):
+        secret = base64.b64encode(b"x" * 32)
+        message = "Test string"
+        rv = PrivatePublisher.encrypt(message, secret=secret)
+        self.assertIsInstance(rv, bytes)
+        self.assertIsInstance(rv.decode("ascii"), str)
+        self.assertIsInstance(base64.urlsafe_b64decode(rv.decode("ascii")), bytes)
+
+    def test_roundtrip_bytes_message(self):
+        secret = base64.b64encode(b"x" * 32)
+        message = "Test string"
+        token = PrivatePublisher.encrypt(message.encode("utf-8"), secret=secret)
+        rv = PrivatePublisher.decrypt(token, secret=secret)
+        self.assertEqual(message, rv)
+
+    def test_roundtrip_string_message(self):
+        secret = base64.b64encode(b"x" * 32)
+        message = "Test string"
+        token = PrivatePublisher.encrypt(message, secret=secret)
+        rv = PrivatePublisher.decrypt(token, secret=secret)
+        self.assertEqual(message, rv)
