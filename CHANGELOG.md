0eb3803214b261e235d1cc53691ac0c3e3de0630
James Gardiner <james@jgardiner.co.uk>
2017-03-15 12:00:07 +0000

    Bump version numbers to 1.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 ### Unreleased
+
+### 1.3.1 2017-03-15
   - Add version number to log
 
 ### 1.3.0 2017-02-16
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import logging
 from app import settings
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
