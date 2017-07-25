b4318433900e6c7ff42652af803b2ce92e7d4d32
Dave Haynes <david.haynes@ons.gov.uk>
2017-07-25 14:50:23 +0100

    Sketch of main function.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 #   encoding: UTF-8
 
-from sdc.rabbit.async_consumer import AsyncConsumer
+from sdc.rabbit import AsyncConsumer
+from sdc.rabbit import MessageConsumer
+from sdc.rabbit import QueuePublisher
+
+from app.response_processor import ResponseProcessor
+
 
 if __name__ == "__main__":
-    pass
+    consumer = AsyncConsumer(
+        durable_queue=True,
+        exchange="/",
+        exchange_type="topic",
+        rabbit_queue="test",
+        [amqp_url]
+    )
+    quarantine_publisher = QueuePublisher(
+        urls=[amqp_url],
+        queue="test_quarantine"
+    )
+    message_consumer = MessageConsumer(
+        consumer, quarantine_publisher,
+        process=ResponseProcessor.process
+    )
