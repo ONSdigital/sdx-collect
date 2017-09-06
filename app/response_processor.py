58c7b9964c58d3ea7863fc221b781a97ce153ca7
Tejas Patel <tejas.patel@ons.gov.uk>
2017-09-06 14:25:56 +0100

    Improve codecov

--- app/response_processor.py
+++ app/response_processor.py
@@ -42,12 +42,6 @@ class ResponseProcessor:
         self.cora_notifications = QueuePublisher(settings.RABBIT_URLS,
                                                  settings.RABBIT_CORA_QUEUE)
 
-        self.cs_notifications = QueuePublisher(settings.RABBIT_URLS,
-                                               settings.RABBIT_CS_QUEUE)
-
-        self.cora_notifications = QueuePublisher(settings.RABBIT_URLS,
-                                                 settings.RABBIT_CORA_QUEUE)
-
     def service_name(self, url=None):
         try:
             parts = url.split('/')
