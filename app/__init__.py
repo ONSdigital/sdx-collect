97281edc1e2e250a26233881fa93ad985a38604f
gemmai95 <gemma.i_95@hotmail.co.uk>
2017-10-16 09:32:49 +0100

    Add config file (#214)
    * Add service config to config file
    
    * Remove secret encoding in config
    
    * Update change log

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
 __version__ = "3.0.1"
+
+# Configure the number of retries attempted before failing call
+session = requests.Session()
+retries = Retry(total=5, backoff_factor=0.1)
+session.mount('http://', HTTPAdapter(max_retries=retries))
+session.mount('https://', HTTPAdapter(max_retries=retries))
