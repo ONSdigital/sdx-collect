2bf81a28474583893acfbb6e5bc0a27d839abfcc
Emilio Ward <emilioward@hotmail.co.uk>
2017-10-24 16:39:42 +0100

    WIP changes

--- tests/test_data.py
+++ tests/test_data.py
@@ -27,7 +27,26 @@ valid_decrypted = '''
 
 invalid_decrypted = '''
 {
-  "abc": "def"
+  "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
+  "type": "uk.gov.ons.edc.eq:surveyresponse",
+  "version": "0.0.1",
+  "origin": "uk.gov.ons.edc.eq",
+  "survey_id": 1,
+  "collection": {
+    "exercise_sid": "hfjdskf",
+    "instrument_id": "0102",
+    "period": "1604"
+  },
+  "metadata": {
+    "user_id": "789473423",
+    "ru_ref": "12345678901A"
+  },
+  "data": {
+    "11": "1/4/2016",
+    "12": "31/10/2016",
+    "20": "1800000",
+    "21": "60000"
+  }
 }
 '''
 
