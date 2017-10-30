b3e3f6bad4c26239aac63711bf0b4b302ce087d2
Warren Bailey <warren@warrenbailey.net>
2017-10-30 16:07:20 +0000

    Fixed pip dependencies

--- app/settings.py
+++ app/settings.py
@@ -2,13 +2,6 @@ import json
 import logging
 import os
 
-CF_DEPLOYMENT = os.getenv("CF_DEPLOYMENT", False)
-
-if CF_DEPLOYMENT:
-    vcap_services = os.getenv("VCAP_SERVICES")
-    parsed_vcap_services = json.loads(vcap_services)
-    rabbit_config = parsed_vcap_services.get('rabbitmq')
-
 
 LOGGING_LEVEL = logging.getLevelName(os.getenv("LOGGING_LEVEL", "DEBUG"))
 LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-collect: %(message)s"
@@ -22,7 +15,7 @@ SDX_VALIDATE_URL = os.getenv("SDX_VALIDATE_URL", "http://sdx-validate:5000/valid
 
 RABBIT_QUEUE = os.getenv('RABBIT_SURVEY_QUEUE', 'survey')
 RABBIT_QUARANTINE_QUEUE = os.getenv('RABBIT_QUARANTINE_QUEUE', 'survey_quarantine')
-RABBIT_EXCHANGE = 'sdx'
+RABBIT_EXCHANGE = 'message'
 
 RABBIT_RRM_RECEIPT_QUEUE = 'rrm_receipt'
 
@@ -30,7 +23,11 @@ SDX_COLLECT_SECRET = os.getenv("SDX_COLLECT_SECRET")
 if SDX_COLLECT_SECRET is not None:
     SDX_COLLECT_SECRET = SDX_COLLECT_SECRET.encode("ascii")
 
-if CF_DEPLOYMENT:
+if os.getenv("CF_DEPLOYMENT", False):
+    vcap_services = os.getenv("VCAP_SERVICES")
+    parsed_vcap_services = json.loads(vcap_services)
+    rabbit_config = parsed_vcap_services.get('rabbitmq')
+
     RABBIT_URL = rabbit_config[0].get('credentials').get('uri')
     RABBIT_URL2 = rabbit_config[1].get('credentials').get('uri') if len(rabbit_config) > 1 else RABBIT_URL
 else:
