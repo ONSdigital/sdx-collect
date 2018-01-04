0e72476d5d70d925b804c473bdb720f823a1e37a
Warren Bailey <warren@warrenbailey.net>
2018-01-04 10:35:27 +0000

    Adding unit test for heartbeat interval

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -15,5 +15,9 @@ class TestSettings(unittest.TestCase):
     def test_cf_settings(self):
         rabbit_url1, rabbit_url2 = settings.parse_vcap_services()
 
-        self.assertEqual("amqp://user:password@168.0.0.1:5672/example_vhost", rabbit_url1)
-        self.assertEqual("amqp://user:password@168.0.0.1:5672/example_vhost", rabbit_url2)
+        self.assertEqual("amqp://user:password@168.0.0.1:5672/example_vhost?heartbeat_interval=5", rabbit_url1)
+        self.assertEqual("amqp://user:password@168.0.0.1:5672/example_vhost?heartbeat_interval=5", rabbit_url2)
+
+    def test_heartbeat(self):
+        self.assertEqual("amqp://rabbit:rabbit@rabbit:5672/%2f?heartbeat_interval=5", settings.RABBIT_URL)
+        self.assertEqual("amqp://rabbit:rabbit@rabbit:5672/%2f?heartbeat_interval=5", settings.RABBIT_URL2)
