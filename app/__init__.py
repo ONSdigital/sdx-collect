6387076ed7c7743eace40c43e28d5f55ff34e21a
Adam Mann <manny160389@hotmail.com>
2020-01-27 12:10:28 +0000

    Update changelog and version (#275)

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
-__version__ = "3.16.1"
+__version__ = "3.17.0"
 
 # Configure the number of retries attempted before failing call
 session = requests.Session()
